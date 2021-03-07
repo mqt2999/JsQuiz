322be2d968de9d93ff21f5084cf724f59b6b164b
Marquell Thompson <mqt060@gmial.com>
2021-03-07 15:02:05 -0600

    final add

--- README.md
+++ README.md
@@ -1,89 +1,12 @@
-# 04 Web APIs: Code Quiz
+#This is a a website that runs a simple java quiz. 
 
-## Your Task
+1. you are prompted with a last player and a start button 
+2. when the start button is clicked the page wil change and display a question and some answers and a timer will start
+3. when you click on a answer it will move to the next question right or wrong but if wrong time is taken from timer and iff right one is added to the score
+4. whem you get to the last question or yourtime has run out you are asked to store you initials and then the screen resets
+5. when the screen resets the you are brought back to the start scren and shown the last player who took the quiz and the score 
+6. at any poin after pressing the start button the last player is a link to reset the quiz questions and bring the screen back to the start 
 
-At some point in your journey to become a full-stack web developer, you’ll likely be asked to complete a coding assessment&mdash;perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. 
-
-To help familiarize you with these tests and allow you to use the skills covered in this unit, this week’s homework invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
-
-This week’s coursework will equip you with all the skills you need to succeed in this assignment.
-
-## User Story
-
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-```
-
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/04-web-apis-homework-demo.gif)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
----
-
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+#Scrren Shots
+![Preview](./assets/images/strScreen.png)
+![Preview](./assets/images/qzScreen.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
+var questioneBoxEl = document.getElementById('questionBox')
+var questionEl = document.getElementById('question')
+var lastScore = document.getElementById("lastScoreLink")
+var answerBoxEl = document.getElementById('answerBox')
+var answerBtnEl = document.querySelectorAll('.btn')
+var a1 = document.getElementById('answer1')
+var a2 = document.getElementById('answer2')
+var a3 = document.getElementById('answer3')
+var a4 = document.getElementById('answer4')
+var timeTxt = document.getElementById('timer');
+var lastScoreBox = document.getElementById('lastScoreList')
+var startBtn = document.getElementById('startBox')
+var lst = document.getElementById("theBest")
+var timeLeft = 20;
+var score = 0;
+var j = 0;
+var clock = true;
+
+bringHome()
+lastPlayer()
+
+//this is a object made of an array with the question and answers for the quiz and displays them when one is added to an defined variable
 var questions = [{
 
-    question: "what is hosting in JavaScript?",
+    question: "1.what is hosting in JavaScript?",
 
     answers: [
         { text: "When everything on the left is assigned to the right?", correct: false },
@@ -12,129 +34,176 @@ var questions = [{
 
 },
 {
-    question: "Is javascript witten static or dynamic?",
+    question: "2.Is javascript witten static or dynamic?",
     answers: [
         { text: "Dynamic", correct: true },
         { text: "both", correct: false },
         { text: "neither", correct: false },
         { text: "none of the above", correct: false }
-    ],
-    getAnswer: function () {
+    ]
+},
+{
+    question: "3.Which way is th ecorrect way to create an array?",
+    answers: [
+        { text: "{12345678}", correct: false },
+        { text: "[gkrjfi fjkrijt 695jij]", correct: false },
+        { text: "{40,55,44,32}", correct: false },
+        { text: "[\"kjdfnk\",\"lkfjgldkj\",\"dflkgjlkfd\"]", correct: true }
+    ]
+},
+{
+    question: "4.How do you write \"Hello World\" in JavaScript? ",
+    answers: [
+        { text: "System.out.print(\"Hello World\");", correct: false },
+        { text: "Document.write(\"Hello World\")", correct: true },
+        { text: "\"Hello World\"", correct: false },
+        { text: "(\"Hello World\");", correct: false }
+    ]
+},
+{
+    question: "5.Choose the Sting variable defined as jjo",
 
-    }
+    answers: [
+        { text: "jjo + \"hello you\" ", correct: false },
+        { text: "var jjo = \" hello there\"",correct:true},
+        { text: "String jjo = \"yo there\"", correct: false},
+        { text: "\" yo there\" ", correct: false}
+    ]
+
+},
+{
+    question: " 6.How do you make a function?",
+    answers: [
+        { text: "function() = myFunction", correct: false },
+        { text: "1 and 3 ", correct: false },
+        { text: "function myFunction()", correct: true },
+        { text: "3 and 1", correct: false }
+
+    ]
 }]
+// this function is for checking if the answer is correct and also checking for the end of the test
+answerBtnEl.forEach(item => {
+    item.addEventListener('click', (event) => {
 
-var questioneBoxEl = document.getElementById('questionBox')
-var questionEl = document.getElementById('question')
-var highScoreBtn = document.getElementById("highScoreLink")
-var answerBoxEl = document.getElementById('answerBox')
-var answerBtnEl = document.querySelectorAll('.btn')
-var a1 = document.getElementById('answer1')
-var a2 = document.getElementById('answer2')
-var a3 = document.getElementById('answer3')
-var a4 = document.getElementById('answer4')
-var timeTxt = document.getElementById('timer');
-var highScoreBox = document.getElementById('highScoreList')
-var startBtn = document.getElementById('startBox')
-var timeLeft = 20;
-var score = 6;
-var q = 0;
-var j = 0;
+        if (j === 5 && event.target.value == 'true') {
+            
+            score++
+            getLastStorage()
+            reset()
+            lastPlayer()
+            clearInterval(timeLeft)
+           }
+        else if (j === 5 && event.target.value == 'false') {
+
+            getLastStorage()
+            reset()
+            lastPlayer()
+            clearInterval(timeLeft)
+           
+        }
+
+        if (event.target.value == 'true' && j < 5) {
+           
+            j++
+            score++
+            getQuestion()
+            answers()
+        }
+        else if (event.target.value == 'false' && j < 5) {
+            timeLeft -= 2
+            j++
+            getQuestion()
+            answers()
+        }
 
 
 
+    })
+})
+//this is a click event that is set to a function that starts the game and timer, it also changes some things on the screen after the button is presssed
 document.getElementById('startGame').addEventListener('click', function startGame() {
 
-    highScoreBox.setAttribute("id", "hide")
+    lastScoreBox.setAttribute("id", "hide")
     startBtn.setAttribute("id", "hide")
     answerBoxEl.classList.remove('hide')
     questioneBoxEl.classList.remove('hide')
     timeTxt.classList.remove('hide')
+    timeTxt.classList.add('timer')
     console.log("whaaaaahhhhhtttt")
-
-
     getQuestion()
     answers()
-
     timer()
-
-
 })
-
-
-
-a1.addEventListener('click', (event) => {
-
-        if (value === 'true') {
-            q++
-            j++
-            answers()
-            getQuestion()
-
-        }
-        else {
-            timeLeft -= 2
-            
-        }
-        
-    
-})
-
-
+// this function displays the list of answers you can choose from with weather its true or false 
 function answers() {
 
-
-    a1.value = questions[q].answers[0].correct
-    a2.value = questions[q].answers[1].correct
-    a3.value = questions[q].answers[2].correct
-    a4.value = questions[q].answers[3].correct
-
-    a1.innerHTML = questions[q].answers[0].text
-    a2.innerHTML = questions[q].answers[1].text
-    a3.innerHTML = questions[q].answers[2].text
-    a4.innerHTML = questions[q].answers[3].text
-
-
-
+    a1.innerHTML = questions[j].answers[0].text
+    a2.innerHTML = questions[j].answers[1].text
+    a3.innerHTML = questions[j].answers[2].text
+    a4.innerHTML = questions[j].answers[3].text
+    
+    a1.value = questions[j].answers[0].correct
+    a2.value = questions[j].answers[1].correct
+    a3.value = questions[j].answers[2].correct
+    a4.value = questions[j].answers[3].correct
 }
-
-function getQuestion(question) {
-questionEl.innerHTML = questions[j].question
-
+//this function displays the question 
+function getQuestion() {
+    questionEl.innerHTML = questions[j].question
 }
-
-
+// this function is for the timer and it also has a function the pomts the use when the time is up
 function timer() {
 
     setInterval(function () {
         if (timeLeft == 0) {
 
             timeTxt.setAttribute('style', 'display:none')
-            clearInterval(timeLeft = 0)
-            askForLet()
-            localStorage.setItem('score', score)
+            clearInterval(timeLeft)
+            getLastStorage()
+            reset()
         }
         timeLeft -= 1
         timeTxt.textContent = timeLeft
-    }, 2000);
+    }, 1000);
 }
+//this fnction gets the last plyer who took the quiz initials and score and saves it to the local storage 
+function getLastStorage(){
 
-function askForLet() {
     var ask = prompt("what are your initials?");
 
-    localStorage.setItem('personIntial', ask)
+    localStorage.setItem("highSco", JSON.stringify([ask,score]))
+ }
+ //this function is getting the intial and score of the last player who played 
+function lastPlayer(){
+
+    var lastpList= JSON.parse(localStorage.getItem("highSco"))
+
+    lst.textContent =" Player: "+ lastpList[0] + " Score: " + lastpList[1];
 }
+//this function holds a click event that rests the page to the starting screen when pressed  
+function bringHome() {
 
-highScoreBtn.addEventListener("click", function () {
+    lastScore.addEventListener("click", function () {
 
-    highScoreBox.setAttribute("id", "highScoreList")
-    startBtn.setAttribute("id", "startbox")
+        startBtn.setAttribute("id", "startBox")
+        lastScoreBox.setAttribute("id", "lastScoreList")
+        answerBoxEl.classList.add('hide')
+        questioneBoxEl.classList.add('hide')
+        timeTxt.classList.add('hide')
+        j = 0
+        q = 0
+        timeLeft=0
+
+    })
+
+}
+// this function resets eveything to the start screen 
+function reset(){
+
+    startBtn.setAttribute("id", "startBox")
+    lastScoreBox.setAttribute("id", "lastScoreList")
     answerBoxEl.classList.add('hide')
     questioneBoxEl.classList.add('hide')
     timeTxt.classList.add('hide')
-
-})
-
-
-
-
+    j = 0
+}
